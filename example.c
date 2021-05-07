2325dfa8ecbe630705ef2316cc4b7fbfa79ba4c5
Yuyang Qi <yuyang.qi@axon.tv>
2021-05-08 01:23:15 +0200

    example cleanup

--- example.c
+++ example.c
@@ -13,18 +13,14 @@ typedef struct {
 
 int main(int argc, char *argv[])
 {
+    hashtable_t *ht = hashtable_create(TEST_SIZE);
     char **keys = calloc(TEST_SIZE, sizeof(*keys));
 
     for (size_t i = 0; i < TEST_SIZE; i++)
     {
         keys[i] = calloc(1, KEY_LEN * sizeof(*keys[i]));
         snprintf(keys[i], KEY_LEN, "key %ld", i);
-    }
 
-    hashtable_t *ht = hashtable_create(TEST_SIZE);
-
-    for (size_t i = 0; i < TEST_SIZE; i++)
-    {
         dummy_data_t d = {i, i+1};
         hashtable_put(ht, keys[i], &d, sizeof(d));
     }
@@ -35,18 +31,18 @@ int main(int argc, char *argv[])
         printf("%s: dummy_a: %d, dummy_b %d\n", keys[i], data->a, data->b);
     }
 
-    printf("Number of elements  : %ld\n", hashtable_get_num_elements(ht));
-    printf("Table size          : %ld\n", hashtable_get_table_size(ht));
-    printf("Number of collisions: %ld\n", hashtable_get_num_collision(ht));
+    printf("\nNumber of elements  : %ld\n", hashtable_get_num_elements(ht));
+    printf("Table size          : %ld\n"  , hashtable_get_table_size(ht));
+    printf("Number of collisions: %ld\n"  , hashtable_get_num_collision(ht));
 
     hashtable_resize(ht, TEST_SIZE * 3);
-    printf("\nResizing 3x...\n");
+    printf("\nResizing 3x original size...\n");
     printf("Number of elements  : %ld\n", hashtable_get_num_elements(ht));
     printf("Table size          : %ld\n", hashtable_get_table_size(ht));
     printf("Number of collisions: %ld\n", hashtable_get_num_collision(ht));
 
     hashtable_resize(ht, TEST_SIZE / 2);
-    printf("\nResizing 0.5x...\n");
+    printf("\nResizing 0.5x original size...\n");
     printf("Number of elements  : %ld\n", hashtable_get_num_elements(ht));
     printf("Table size          : %ld\n", hashtable_get_table_size(ht));
     printf("Number of collisions: %ld\n", hashtable_get_num_collision(ht));
